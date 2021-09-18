ddec7260db33b43b77370959afd2580de8c1989f
Jesus Cervantes <jesus.cervantes0801@gmail.com>
2021-09-18 18:36:46 -0500

    initial config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 const App = () => {
   return (
     <div className="App">
-      <h1>Hola</h1>
+      <h1>Header 1</h1>
+      <h2>Subtitle</h2>
+      <p>Buenas tardes somos el equipo 1 vamos a ver</p>
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Inicie Sesion</h1>
+    </div>
+  );
+};
+
+export default Home;

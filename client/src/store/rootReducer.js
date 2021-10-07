a43c045dfdcefb111482b511caa2075512a815d5
Jesus Cervantes <jesus.cervantes0801@gmail.com>
2021-10-07 17:45:55 -0500

    fixed root saga

--- client/src/store/rootReducer.js
+++ client/src/store/rootReducer.js
@@ -3,4 +3,4 @@ import auth from "./auth/reducer";
 import user from "./user/reducer";
 import room from "./room/reducer";
 
-export default combineReducers({ auth, user, room });
+export default combineReducers({ auth, user });
--- client/src/store/rootSaga.js
+++ client/src/store/rootSaga.js
@@ -1,8 +1,8 @@
-import { all, call } from "redux-saga/effects"
+import { fork, all, call } from "redux-saga/effects"
 
 import authSaga from './auth/saga'
 import userSaga from './user/saga'
 
 export default function * rootSaga() {
-	yield all([call(authSaga, userSaga)])
+	yield all([fork(authSaga), fork(userSaga)])
 }
